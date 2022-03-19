83f78293f3b3c8e58ff4a697a404e47171ab8854
spf <1203223648@qq.com>
2022-03-19 20:49:37 +0800

    first IDEA commit

---
+++ src/main/java/com/spf/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.spf.git;
+
+/**
+ * @author spf
+ * @create 2022-03-19 20:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
